d1481338ca00ab75dfd9832cdc2c5d522d6c2df0
Marlon Prado <marlonprado04@gmail.com>
2023-10-18 17:17:19 -0300

    feat: add card-pricing html, css, ts -> add to card component

--- store/src/app/components/card/card-pricing/card-pricing.component.ts
+++ store/src/app/components/card/card-pricing/card-pricing.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-card-pricing',
-  templateUrl: './card-pricing.component.html',
-  styleUrls: ['./card-pricing.component.css']
+	selector: 'app-card-pricing',
+	templateUrl: './card-pricing.component.html',
+	styleUrls: ['./card-pricing.component.css']
 })
 export class CardPricingComponent implements OnInit {
 
-  constructor() { }
+	// Variáveis para armazenar o preço e a plataforma
+	@Input()
+	gameType: string = "Digital PS4";
+	@Input()
+	gamePrice: string = "R$ 299,90"
 
-  ngOnInit(): void {
-  }
+	constructor() { }
+
+	ngOnInit(): void {
+	}
 
 }
