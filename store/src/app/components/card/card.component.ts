e983701f091059963f7efe8e725ccfc2dd1044bd
Marlon Prado <marlonprado04@gmail.com>
2023-10-18 17:45:20 -0300

    feat: add repassing of informations

--- store/src/app/components/card/card.component.ts
+++ store/src/app/components/card/card.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+	selector: 'app-card',
+	templateUrl: './card.component.html',
+	styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+	// Variável para armazenar o caminho da imagem
+	@Input()
+	gameCover: string = "";
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+	// Cria variável para armazenar informação da label
+	@Input()
+	gameLabel: string = "";
+
+
+	// Variáveis para armazenar o preço e a plataforma
+	@Input()
+	gameType: string = "Digital PS4";
+	@Input()
+	gamePrice: string = "R$ 299,90"
+
+	constructor() { }
+
+	ngOnInit(): void {
+	}
 
 }
